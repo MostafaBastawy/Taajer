f955fee5c44b01de9866bfa8dedd7d5b8dfe9e68
mostafabastawy <mostafa.bastawy@gmail.com>
2022-02-27 15:56:11 +0200

    register ui updated

--- lib/modules/authentication/register/registeration3.dart
+++ lib/modules/authentication/register/registeration3.dart
@@ -1,10 +1,270 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:taajer/modules/authentication/authentication_cubit/authentication_cubit.dart';
+import 'package:taajer/modules/authentication/authentication_cubit/authentication_states.dart';
+import 'package:taajer/shared/styles/colors.dart';
 
 class RegisterScreen3 extends StatelessWidget {
-  const RegisterScreen3({Key? key}) : super(key: key);
+  var otpVerificationNumber1Controller = TextEditingController();
+  var otpVerificationNumber2Controller = TextEditingController();
+  var otpVerificationNumber3Controller = TextEditingController();
+  var otpVerificationNumber4Controller = TextEditingController();
+  RegisterScreen3({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    AuthenticationCubit cubit = AuthenticationCubit.get(context);
+    return BlocBuilder<AuthenticationCubit, AuthenticationStates>(
+      builder: (BuildContext context, state) => Scaffold(
+        appBar: AppBar(
+          leading: const Icon(
+            Icons.arrow_back_sharp,
+          ),
+          title: const Text('OTP Verification'),
+        ),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 26.h),
+            child: Column(
+              children: [
+                Center(
+                  child: SvgPicture.asset(
+                    'assets/images/otp-verification.svg',
+                    height: 273.h,
+                    width: 273.w,
+                  ),
+                ),
+                SizedBox(height: 44.h),
+                Text(
+                  'Verify Your Mobile',
+                  style: TextStyle(
+                    color: primaryBlack,
+                    fontWeight: FontWeight.w700,
+                    fontSize: 24.sp,
+                    letterSpacing: -0.65,
+                  ),
+                ),
+                SizedBox(height: 18.h),
+                Text(
+                  'Please enter the 4 digit code sent to',
+                  style: TextStyle(
+                    color: primaryBlack,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 15.sp,
+                    letterSpacing: -0.17,
+                  ),
+                ),
+                SizedBox(height: 10.h),
+                Text(
+                  '+973 5536 7654',
+                  style: TextStyle(
+                    color: primaryBlack,
+                    fontWeight: FontWeight.w600,
+                    fontSize: 15.sp,
+                    letterSpacing: -0.17,
+                  ),
+                ),
+                SizedBox(height: 20.h),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      padding: EdgeInsets.symmetric(horizontal: 16.w),
+                      width: 50.w,
+                      height: 50.h,
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          width: 1.0,
+                          color: const Color(0xAFE2E4E8),
+                        ),
+                        borderRadius: BorderRadius.circular(6.r),
+                      ),
+                      child: TextFormField(
+                        controller: otpVerificationNumber1Controller,
+                        keyboardType: TextInputType.number,
+                        inputFormatters: [
+                          FilteringTextInputFormatter.digitsOnly
+                        ],
+                        maxLines: 1,
+                        obscureText: false,
+                        onChanged: (value) {},
+                        onFieldSubmitted: (value) {},
+                        validator: (value) {},
+                        onTap: () {},
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: '',
+                          hintStyle: TextStyle(
+                            fontSize: 16.sp,
+                            fontWeight: FontWeight.w500,
+                            color: const Color(0xAFB2B7C2),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 15.w),
+                    Container(
+                      padding: EdgeInsets.symmetric(horizontal: 16.w),
+                      width: 50.w,
+                      height: 50.h,
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          width: 1.0,
+                          color: const Color(0xAFE2E4E8),
+                        ),
+                        borderRadius: BorderRadius.circular(6.r),
+                      ),
+                      child: TextFormField(
+                        controller: otpVerificationNumber2Controller,
+                        keyboardType: TextInputType.number,
+                        inputFormatters: [
+                          FilteringTextInputFormatter.digitsOnly
+                        ],
+                        maxLines: 1,
+                        obscureText: false,
+                        onChanged: (value) {},
+                        onFieldSubmitted: (value) {},
+                        validator: (value) {},
+                        onTap: () {},
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: '',
+                          hintStyle: TextStyle(
+                            fontSize: 16.sp,
+                            fontWeight: FontWeight.w500,
+                            color: const Color(0xAFB2B7C2),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 15.w),
+                    Container(
+                      padding: EdgeInsets.symmetric(horizontal: 16.w),
+                      width: 50.w,
+                      height: 50.h,
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          width: 1.0,
+                          color: const Color(0xAFE2E4E8),
+                        ),
+                        borderRadius: BorderRadius.circular(6.r),
+                      ),
+                      child: TextFormField(
+                        controller: otpVerificationNumber3Controller,
+                        keyboardType: TextInputType.number,
+                        inputFormatters: [
+                          FilteringTextInputFormatter.digitsOnly
+                        ],
+                        maxLines: 1,
+                        obscureText: false,
+                        onChanged: (value) {},
+                        onFieldSubmitted: (value) {},
+                        validator: (value) {},
+                        onTap: () {},
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: '',
+                          hintStyle: TextStyle(
+                            fontSize: 16.sp,
+                            fontWeight: FontWeight.w500,
+                            color: const Color(0xAFB2B7C2),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 15.w),
+                    Container(
+                      padding: EdgeInsets.symmetric(horizontal: 16.w),
+                      width: 50.w,
+                      height: 50.h,
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          width: 1.0,
+                          color: const Color(0xAFE2E4E8),
+                        ),
+                        borderRadius: BorderRadius.circular(6.r),
+                      ),
+                      child: TextFormField(
+                        controller: otpVerificationNumber4Controller,
+                        keyboardType: TextInputType.number,
+                        inputFormatters: [
+                          FilteringTextInputFormatter.digitsOnly
+                        ],
+                        maxLines: 1,
+                        obscureText: false,
+                        onChanged: (value) {},
+                        onFieldSubmitted: (value) {},
+                        validator: (value) {},
+                        onTap: () {},
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: '',
+                          hintStyle: TextStyle(
+                            fontSize: 16.sp,
+                            fontWeight: FontWeight.w500,
+                            color: const Color(0xAFB2B7C2),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 25.h),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Didn’t receive OTP?',
+                      style: TextStyle(
+                        color: const Color(0xFFA4A5A9),
+                        fontWeight: FontWeight.w400,
+                        fontSize: 15.sp,
+                        letterSpacing: -0.17,
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Resend',
+                        style: TextStyle(
+                          color: primaryBlue,
+                          fontWeight: FontWeight.w700,
+                          fontSize: 15.sp,
+                          letterSpacing: -0.17,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 50.h),
+                Container(
+                  width: 344.w,
+                  height: 46.h,
+                  decoration: BoxDecoration(
+                    color: primaryBlue,
+                    borderRadius: BorderRadius.circular(6.r),
+                  ),
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Confirm & Next',
+                      style: TextStyle(
+                        fontSize: 15.sp,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w700,
+                        letterSpacing: -0.165,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }

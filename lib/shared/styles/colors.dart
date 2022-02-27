2ac0b6cbf98c0aaef0fa2a063fadf55ff22d33fd
mostafabastawy <mostafa.bastawy@gmail.com>
2022-02-27 10:17:24 +0200

    register ui updated

--- lib/shared/styles/colors.dart
+++ lib/shared/styles/colors.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
-const Color primaryBlue = Color(0xAF0761FD);
-const Color primaryRed = Color(0xAFED1C24);
-const Color primaryYellow = Color(0xAFF1C400);
-const Color primaryBlack = Color(0xAF1B1D28);
+const Color primaryBlue = Color(0xFF0761FD);
+const Color primaryRed = Color(0xFFED1C24);
+const Color primaryYellow = Color(0xFFF1C400);
+const Color primaryBlack = Color(0xFF1B1D28);
 
-const Color secondaryGreen = Color(0xAF0BD3C4);
-const Color secondaryRed = Color(0xAFF15A24);
+const Color secondaryGreen = Color(0xFF0BD3C4);
+const Color secondaryRed = Color(0xFFF15A24);
 
-const Color errorRed = Color(0xAFFF3236);
+const Color errorRed = Color(0xFFFF3236);

3db376a723cac3830fcb60c4b052788a24a4c1fb
mostafabastawy <mostafa.bastawy@gmail.com>
2022-03-09 22:45:37 +0200

    complete register ui

--- lib/shared/components/bottom_nav_bar/bottom_nav_bar.dart
+++ lib/shared/components/bottom_nav_bar/bottom_nav_bar.dart
@@ -2,62 +2,76 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:taajer/app_cubit/app_cubit.dart';
-import 'package:taajer/shared/styles/colors.dart';
 
 List<BottomNavigationBarItem> bottomNavBarItem(BuildContext context) => [
       BottomNavigationBarItem(
-        icon: SvgPicture.asset(
-          'assets/images/nav-item1.svg',
-          height: 24.h,
-          width: 24.w,
-          color: AppCubit.get(context).currentIndex == 0
-              ? figmaPrimaryBlue
-              : const Color(0xFFB2B7C2),
-        ),
+        icon: AppCubit.get(context).currentIndex == 0
+            ? SvgPicture.asset(
+                'assets/images/bottom_nav_bar/selected-nav-item1.svg',
+                height: 24.h,
+                width: 24.w,
+              )
+            : SvgPicture.asset(
+                'assets/images/bottom_nav_bar/nav-item1.svg',
+                height: 24.h,
+                width: 24.w,
+              ),
         label: 'Browse',
       ),
       BottomNavigationBarItem(
-        icon: SvgPicture.asset(
-          'assets/images/nav-item2.svg',
-          height: 24.h,
-          width: 24.w,
-          color: AppCubit.get(context).currentIndex == 1
-              ? figmaPrimaryBlue
-              : const Color(0xFFB2B7C2),
-        ),
+        icon: AppCubit.get(context).currentIndex == 1
+            ? SvgPicture.asset(
+                'assets/images/bottom_nav_bar/selected-nav-item2.svg',
+                height: 24.h,
+                width: 24.w,
+              )
+            : SvgPicture.asset(
+                'assets/images/bottom_nav_bar/nav-item2.svg',
+                height: 24.h,
+                width: 24.w,
+              ),
         label: 'Wishlist',
       ),
       BottomNavigationBarItem(
-        icon: SvgPicture.asset(
-          'assets/images/nav-item3.svg',
-          height: 24.h,
-          width: 24.w,
-          color: AppCubit.get(context).currentIndex == 2
-              ? figmaPrimaryBlue
-              : const Color(0xFFB2B7C2),
-        ),
+        icon: AppCubit.get(context).currentIndex == 2
+            ? SvgPicture.asset(
+                'assets/images/bottom_nav_bar/selected-nav-item3.svg',
+                height: 24.h,
+                width: 24.w,
+              )
+            : SvgPicture.asset(
+                'assets/images/bottom_nav_bar/nav-item3.svg',
+                height: 24.h,
+                width: 24.w,
+              ),
         label: 'Orders',
       ),
       BottomNavigationBarItem(
-        icon: SvgPicture.asset(
-          'assets/images/nav-item4.svg',
-          height: 24.h,
-          width: 24.w,
-          color: AppCubit.get(context).currentIndex == 3
-              ? figmaPrimaryBlue
-              : const Color(0xFFB2B7C2),
-        ),
+        icon: AppCubit.get(context).currentIndex == 3
+            ? SvgPicture.asset(
+                'assets/images/bottom_nav_bar/selected-nav-item4.svg',
+                height: 24.h,
+                width: 24.w,
+              )
+            : SvgPicture.asset(
+                'assets/images/bottom_nav_bar/nav-item4.svg',
+                height: 24.h,
+                width: 24.w,
+              ),
         label: 'Cart',
       ),
       BottomNavigationBarItem(
-        icon: SvgPicture.asset(
-          'assets/images/nav-item5.svg',
-          height: 24.h,
-          width: 24.w,
-          color: AppCubit.get(context).currentIndex == 4
-              ? figmaPrimaryBlue
-              : const Color(0xFFB2B7C2),
-        ),
+        icon: AppCubit.get(context).currentIndex == 4
+            ? SvgPicture.asset(
+                'assets/images/bottom_nav_bar/selected-nav-item5.svg',
+                height: 24.h,
+                width: 24.w,
+              )
+            : SvgPicture.asset(
+                'assets/images/bottom_nav_bar/nav-item5.svg',
+                height: 24.h,
+                width: 24.w,
+              ),
         label: 'Profile',
       ),
     ];
